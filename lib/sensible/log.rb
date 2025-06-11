795b0b6221f8b3b93b97d8b8bd88ac35e570b2d0
dasmikko <dasmikko@gmail.com>
2025-06-11 21:53:09 +0200

    Improve log, allow for overwriting the line

--- lib/sensible/log.rb
+++ lib/sensible/log.rb
@@ -1,33 +1,46 @@
 require 'tty-prompt'
+require 'tty-spinner'
 require 'pastel'
 
 module Sensible
   $pastel = Pastel.new
 
   class Logger 
-    def self.log(message)
-      puts message
+    def self.log(message, indent = 0, use_print: false)
+        puts message
     end
 
-    def self.success(message, indent = 0)
+    def self.success(message, indent = 0, use_print: false)
       spaceIndent = ""
       indent.times { |i| spaceIndent << " " }
 
-      puts "#{spaceIndent}#{$pastel.green("✔")} #{message}"
+      if use_print
+        print "#{spaceIndent}#{$pastel.green("✔")} #{message}"
+      else      
+        puts "#{spaceIndent}#{$pastel.green("✔")} #{message}"
+      end
     end
 
-    def self.info(message, indent = 0)
+    def self.info(message, indent = 0, use_print: false)
       spaceIndent = ""
       indent.times { |i| spaceIndent << " " }
-
-      puts "#{spaceIndent}#{$pastel.blue("i")} #{message}"
+ 
+      if use_print
+        print "#{spaceIndent}#{$pastel.blue("i")} #{message}"
+      else      
+        puts "#{spaceIndent}#{$pastel.blue("i")} #{message}"
+      end
     end
 
-    def self.danger(message, indent = 0)
+    def self.danger(message, indent = 0, use_print: false)
       spaceIndent = ""
       indent.times { |i| spaceIndent << " " }
-
-      puts "#{spaceIndent}#{$pastel.red("✘")} #{message}"
+      
+      if use_print
+        print "#{spaceIndent}#{$pastel.red("✘")} #{message}"
+      else      
+        puts "#{spaceIndent}#{$pastel.red("✘")} #{message}"
+      end      
     end
 
     def self.yes?(message, indent = 0)
