a6f720bc8df1ae946c1d9a9aa1288752324f2b42
dasmikko <dasmikko@gmail.com>
2025-06-11 20:29:07 +0200

    Fix indentation

--- lib/sensible/log.rb
+++ lib/sensible/log.rb
@@ -2,42 +2,41 @@ require 'tty-prompt'
 require 'pastel'
 
 module Sensible
-    $pastel = Pastel.new
+  $pastel = Pastel.new
 
-    class Logger 
-        def self.log(message)
-            puts message
-        end
-
-        def self.success(message, indent = 0)
-            spaceIndent = ""
-            indent.times { |i| spaceIndent << " " }
-
-            puts "#{spaceIndent}#{$pastel.green("✔")} #{message}"
-        end
-
-        def self.info(message, indent = 0)
-            spaceIndent = ""
-            indent.times { |i| spaceIndent << " " }
-
-            puts "#{spaceIndent}#{$pastel.blue("i")} #{message}"
-        end
-
-        def self.danger(message, indent = 0)
-            spaceIndent = ""
-            indent.times { |i| spaceIndent << " " }
-
-            puts "#{spaceIndent}#{$pastel.red("✘")} #{message}"
-        end
-
-        def self.yes?(message, indent = 0)
-            prompt = TTY::Prompt.new
-
-            spaceIndent = ""
-            indent.times { |i| spaceIndent << " " }
-
-            prompt.yes?("#{spaceIndent}#{message}")
-        end
-
+  class Logger 
+    def self.log(message)
+      puts message
     end
+
+    def self.success(message, indent = 0)
+      spaceIndent = ""
+      indent.times { |i| spaceIndent << " " }
+
+      puts "#{spaceIndent}#{$pastel.green("✔")} #{message}"
+    end
+
+    def self.info(message, indent = 0)
+      spaceIndent = ""
+      indent.times { |i| spaceIndent << " " }
+
+      puts "#{spaceIndent}#{$pastel.blue("i")} #{message}"
+    end
+
+    def self.danger(message, indent = 0)
+      spaceIndent = ""
+      indent.times { |i| spaceIndent << " " }
+
+      puts "#{spaceIndent}#{$pastel.red("✘")} #{message}"
+    end
+
+    def self.yes?(message, indent = 0)
+      prompt = TTY::Prompt.new
+
+      spaceIndent = ""
+      indent.times { |i| spaceIndent << " " }
+
+      prompt.yes?("#{spaceIndent}#{message}")
+    end
+  end
 end
--- lib/sensible/package.rb
+++ lib/sensible/package.rb
@@ -4,32 +4,32 @@ require 'tty-prompt'
 require 'pastel'
 
 module Sensible
-    class Package
-        attr_reader :sensible
-        attr_reader :name
-        attr_reader :check
+  class Package
+    attr_reader :sensible
+    attr_reader :name
+    attr_reader :check
 
-        def initialize(packageHash, sensible)
-            @name = packageHash['name']
-            @sensible = sensible
-        end
-
-
-        # Check if the package is installed
-        def do_check
-            result = `rpm -q #{@name}`
-            
-            if result.include? 'is not installed'
-                return false
-            else 
-                return true
-            end           
-        end
-        
-        # Install the package
-        def do_install
-            system("sudo dnf install -y #{@name}")
-            return $?.success?
-        end
+    def initialize(packageHash, sensible)
+      @name = packageHash['name']
+      @sensible = sensible
     end
+
+
+    # Check if the package is installed
+    def do_check
+      result = `rpm -q #{@name}`
+      
+      if result.include? 'is not installed'
+        return false
+      else 
+        return true
+      end           
+    end
+    
+    # Install the package
+    def do_install
+      system("sudo dnf install -y #{@name}")
+      return $?.success?
+    end
+  end
 end
