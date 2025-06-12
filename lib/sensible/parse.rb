9bab2690c06e9de1ea3f1e9d5c7a25e6bcb21520
dasmikko <dasmikko@gmail.com>
2025-06-12 07:53:21 +0200

    Pass sensible correctly to the parse function

--- lib/sensible/parse.rb
+++ lib/sensible/parse.rb
@@ -3,10 +3,10 @@ module Sensible
   class Parse
     
     # Parse the package list from sensible.yml
-    def self.parse_sensible_packages(sensible_hash_list)
+    def self.parse_sensible_packages(sensible_hash_list, sensible)
       list = []
       for pkg in sensible_hash_list
-        list.append(Package.new(pkg, self))
+        list.append(Package.new(pkg, sensible))
       end
       return list
     end
