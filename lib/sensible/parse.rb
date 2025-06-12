61e44aca0225c21ffba5de685a83c3f813582f2b
dasmikko <dasmikko@gmail.com>
2025-06-12 09:01:54 +0200

    Add requires

--- lib/sensible/parse.rb
+++ lib/sensible/parse.rb
@@ -1,3 +1,5 @@
+require_relative 'task'
+require_relative 'package'
 
 module Sensible
   class Parse
@@ -7,6 +9,15 @@ module Sensible
       list = []
       for pkg in sensible_hash_list
         list.append(Package.new(pkg, sensible))
+      end
+      return list
+    end
+
+    # Parse the task list from sensible.yml
+    def self.parse_sensible_tasks(sensible_hash_list, sensible)
+      list = []
+      for task in sensible_hash_list
+        list.append(Task.new(tash, sensible))
       end
       return list
     end
